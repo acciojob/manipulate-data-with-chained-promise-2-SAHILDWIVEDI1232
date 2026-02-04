9ef10c321701eccae834a176e220e15979d8a9b1
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-02-04 12:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const outputDiv = document.getElementById("output");
+function getArr(){
+return new Promise(resolve)=>{
+	setTimeout(()=>{
+		resolve([1,2,3,4]);
+		
+	},3000)
+}};
+getArr().then((arr)=>{
+const evens=arr.filter(num=>num%2==0);
+return new Promise(reoslve)=>
+ outputDiv.textContent = evens.join(",");
+        resolve(evens);
+      }, 1000);
+ });
+})
+.then((evens)=>{
+	const multiplied =evens.map(num=>num*2);
+
+	return new Promise((resolve)=>{
+		outputDiv.textContent = multiplied.join(",");
+        resolve(multiplied);
+      }, 2000);
+	})
+})
